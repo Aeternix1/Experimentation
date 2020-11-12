46f731fc30a4c7371e51a9453ebfcf8f6cb75115
Ajay <z5109790@cse.unsw.edu.au>
2020-11-12 17:55:23 +1100

    added folder structure

---
+++ C_tutorial/bit_representation_integers.c
@@ -0,0 +1,19 @@
+//Bit Representation of integers
+//A small program used to understand the bit representation of integers
+
+
+#include <stdio.h>
+
+
+int main (int argc, char **argv) 
+{
+    
+    int input;
+    printf("Please enter an integer value\n"); 
+    scanf("%d", &input); 
+
+
+    printf("The input %d represented in bits equals", input);
+
+
+}
