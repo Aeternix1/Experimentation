0243c7c0f850fb302649afcb645c1ab5c2d99a60
Varghese Kallukalam <32807651+Aeternix1@users.noreply.github.com>
2021-01-03 00:53:30 +1100

    There's a problem with output formatting

--- videos.bat
+++ videos.bat
@@ -1,4 +1,5 @@
 ECHO ON
 
+REM Need to figure out how to make the initial command a variable
 C:\youtube-dl\youtube-dl.exe --ignore-errors --format best https://www.youtube.com/watch?v=-0B3w2uO0b0
 C:\youtube-dl\youtube-dl.exe --ignore-errors --format best https://www.youtube.com/watch?v=s2OVGhHg9Rc
