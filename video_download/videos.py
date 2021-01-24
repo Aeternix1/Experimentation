95c0331a6ce663ee9fe350f86638f34177e44bab
root <root@DESKTOP-J14084V.localdomain>
2021-01-24 13:04:47 +1100

    need to switch machines to test code

--- video_download/videos.py
+++ video_download/videos.py
@@ -1,7 +1,8 @@
 import os 
 import sys
 
-cmd_string = 'C:\youtube-dl\youtube-dl.exe --ignore-errors --format best --output '
+single_dl = 'C:\youtube-dl\youtube-dl.exe --ignore-errors --format best --output ' 
+multiple_dl = 'C:\youtube-dl
 #open the text file required
 file = open(sys.argv[1])
 Lines = file.readlines() 
@@ -11,7 +12,8 @@ videos = {}
 for line in Lines:
     line = line.strip()
     if "https:" not in line:
-        out = '"' + line + '.%(ext)s"'
+        out = '"' + line + '.%(ext)s"' 
+    else if "playlist"
     else:
         tmp = " ".join([cmd_string, out, line.strip()])
         os.system(tmp) 
