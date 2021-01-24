1c40439eff852c21da57540896237c27eb2fbfef
root <root@DESKTOP-J14084V.localdomain>
2021-01-24 21:06:44 +1100

    windows and unix versions with playlists

---
+++ video_download/vd_unix.py
@@ -0,0 +1,23 @@
+import os 
+import sys
+
+single_dl = 'youtube-dl --ignore-errors --format best --output ' 
+pl_dl = 'youtube-dl --ignore-errors -f best --output "%(title)s.%(ext)s" --yes-playlist '
+
+#open the text file required
+file = open(sys.argv[1])
+Lines = file.readlines() 
+
+videos = {}
+
+for line in Lines:
+    line = line.strip()
+    if "https:" not in line: 
+        title = '"' + line + '.%(ext)s"'
+    else:  
+        if "playlist" in line:
+            tmp = " ".join([pl_dl, line.strip()])
+        else: 
+            tmp = " ".join([single_dl, title, line.strip()])
+        os.system(tmp) 
+        print(tmp)
