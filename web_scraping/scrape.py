c18c33013c47da50769831d1cc2953f313ef73a3
root <root@DESKTOP-J14084V.localdomain>
2021-01-28 17:48:48 +1100

    can loop through the summary table values

--- web_scraping/scrape.py
+++ web_scraping/scrape.py
@@ -36,7 +36,7 @@ import requests
 # print(atag.string) 
 
 
-#Exercise : Print the title and summary of Yoel Romero's wikipedia Page
+##Exercise : Print the title and summary of Yoel Romero's wikipedia Page
 # url = 'https://en.wikipedia.org/wiki/Yoel_Romero'
 # requests.get(url)
 # source = requests.get(url)
@@ -49,25 +49,37 @@ import requests
 # summary = soup.p
 # print(summary.text)
 
-#Exercise : Loop through multiple fighters and retrieve the title + wiki summary 
+# Exercise : Loop through multiple fighters and retrieve the title + wiki
+# summary + summary table
 wiki_url = 'https://en.wikipedia.org/wiki/' 
-fighters = ["Yoel Romero", "Dustin Poirier", "Connor McGregor"]  
+fighters = ["Yoel Romero", "Dustin Poirier", "Connor McGregor"]   
+
+# Given a fighter name, produce it's wikipedia URL
 for fighter in fighters: 
     tmp = fighter.split()
     fighter = "_".join(tmp)   
     url = wiki_url + fighter  
-    print(url)
-
+    
+    #Parse the URL and access the heading, table and opening summary paragraph
     requests.get(url)
     source = requests.get(url)
     soup = BeautifulSoup(source.text, 'lxml')
     title = soup.h1
     print(title.text)
-    summary = soup.p
-    print(summary.text)
-
-
-
+    summary = soup.find_all("p")
+    print(summary[0].text)
+    print(summary[1].text)  
+    table = soup.find("table", class_="infobox vcard")   
+    table_heading = soup.find_all("th", scope="row")
+    table_value = soup.find_all("td") 
+    
+    #Print off the tr and th
+    for x in range(0, 20): 
+        print(table_heading[x].text)
+        print(table_value[x+1].text) 
+    # print(table_heading)  
+    # print(table_value)
+    # Need consistent formatting to scrape without introducing exception logic 
 
 
 #########################  FIND  ###########################################
